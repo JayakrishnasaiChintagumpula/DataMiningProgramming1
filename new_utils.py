41907e8370af0816062f8aa0a8c3919bc89f51c7
JayakrishnasaiChintagumpula <146905031+JayakrishnasaiChintagumpula@users.noreply.github.com>
2024-02-22 10:33:56 -0500

    Update new_utils.py

--- new_utils.py
+++ new_utils.py
@@ -10,18 +10,6 @@
   
      import new_utils as nu
 """
-"""
-   Use to create your own functions for reuse
-   across the assignment
-
-   Inside part_1_template_solution.py,
-
-     import new_utils
-
-    or
-
-     import new_utils as nu
-"""
 
 import numpy as np
 from typing import Type, Dict
